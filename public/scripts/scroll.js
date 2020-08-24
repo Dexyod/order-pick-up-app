9bd68c1f21d29eb37d0a6e85645ab88a4e7e1d24
Dexyod <dexyod43@gmail.com>
2020-08-24 22:21:35 +0000

    added scroll to top and open cart button and functionality

---
+++ public/scripts/scroll.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  //show button to go to textarea and autofocus if scrolled down the page
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $("#scroll-btn").addClass("show");
+      $("#scroll-btn").fadeIn();
+    } else {
+      $("#scroll-btn").fadeOut();
+    }
+  });
+  $("#scroll-btn").on("click", function (event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, 300, function () {
+      $("#ModalCart").modal();
+    });
+  });
+});
