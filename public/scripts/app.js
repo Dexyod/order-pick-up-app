d67a15b7f81f6a080a84addcd42cb0e0688571f2
Jay Martens <martensjay93@gmail.com>
2020-08-25 15:15:15 -0400

    Made the template and the call for menu items pt.2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,18 @@
 $(() => {
+    const createMenuItem = (item) => {
+    return (`
+    <div class="card card-body">
+    <span class="float-right font-weight-bold">$${(item.price / 100).toFixed(2)}</span>
+    <h6 class="text-truncate">${item.name}</h6>
+    <img src="${item.photo_url}" alt=""
+      class="img-thumbnail">
+    <p class="small item-description">
+      ${item.description}
+    </p>
+    </div>
+    `);
+  };
+
   $('#register-form').submit(function (event) {
     event.preventDefault();
     const formData = $(this).serialize();
@@ -41,4 +55,12 @@ $(() => {
       $("#login-options").load(location.href + " #login-options");
     });
   });
+  $.ajax('/api/items/', {
+    method: 'GET'
+  })
+  .then(function (response) {
+    response.forEach(item => $('#menu-container').prepend(createMenuItem(item)));
+    console.log(response[0]);
+  })
+
 });
