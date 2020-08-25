3584591238980c97578a649eb43ef93b71d5afa7
Jay Martens <martensjay93@gmail.com>
2020-08-25 14:09:18 -0400

    Finished making the user log in stuff dynamic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,6 +9,10 @@ $(() => {
     .then(function (response) {
       console.log(response);
       $('#ModalRegister').modal('toggle');
+      $("#login-options").load(location.href + " #login-options");
+      $( '#register-form' ).each(function(){
+        this.reset();
+    });
     });
   });
   $('#login-form').submit(function (event) {
@@ -21,15 +25,20 @@ $(() => {
     .then(function (response) {
       console.log(response);
       $('#ModalLogin').modal('toggle');
+      $("#login-options").load(location.href + " #login-options");
+      $( '#login-form' ).each(function(){
+        this.reset();
+    });
     });
   });
-  $('#logout').click(function (event) {
+  $('#login-options').on('click', '#logout', function (event) {
     event.preventDefault();
     $.ajax('/api/users/logout', {
       method: 'POST'
     })
     .then(function (response) {
       console.log(response);
+      $("#login-options").load(location.href + " #login-options");
     });
   });
 });
