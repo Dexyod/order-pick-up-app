24df71656b090472a92ee2c0017103fa843c8485
Patrick Mitchener <pmitchener@rogers.com>
2020-08-25 21:45:04 +0000

    Added category to items table. Delete line_no from order_details table.

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -1,7 +1,11 @@
-  INSERT INTO items (name, description, price, photo_url)
-  VALUES ('Canadian Chicken & Bacon Caesar Salad', 'Romaine tossed with Caesar dressing, croutons, shaved parmesan cheese, grilled or crispy chicken breast, crispy bacon, served with cheese crackers', 1400, 'images/chicken_bacon_caesar.jpg'),
-  ('You''re Bacon Me Crazy BLT', '1/2 lb Mountain of crispy bacon, lettuce, tomato, mayonnaise, melted cheddar, choice of bread, baby dills, bacon & cheese potato puffs', 1500, 'images/bacon_me_crazy_blt.jpg'),
-  ('Grilled Cheese, Bacon and Oven-Dried Tomato Sandwich', 'This is grilled cheese and whole nuther level', 1200, 'images/Grilled-Cheese-Bacon-Oven-Dried-Tomato-Sandwich.jpg'),
-  ('Chicken & Bacon Tart', 'If you’re one of those folks who think bacon is a food group, you’ll love this weeknight-friendly twist with a sweet and spicy kick from jalapeno jelly.', 1100, 'images/chicken-bacon-tart.jpg'),
-  ('Bacon meets bun', 'You will be truly satified with the work of bacon', 900, '/images/best-bacon-burger.jpg'),
-  ('Bacon and eggs', 'A classic breakfast of eggs your way, bacon, toast, and home fries.', 895, '/images/two-eggs-sunny-side-up.jpg')
+  /* item categories
+  Starters, Lunch, Dinner, Drinks
+  */
+
+  INSERT INTO items (name, description, category, price, photo_url)
+  VALUES ('Canadian Chicken & Bacon Caesar Salad', 'Romaine tossed with Caesar dressing, croutons, shaved parmesan cheese, grilled or crispy chicken breast, crispy bacon, served with cheese crackers', 'Starters', 1400, 'images/chicken_bacon_caesar.jpg'),
+  ('You''re Bacon Me Crazy BLT', '1/2 lb Mountain of crispy bacon, lettuce, tomato, mayonnaise, melted cheddar, choice of bread, baby dills, bacon & cheese potato puffs', 'Lunch', 1500, 'images/bacon_me_crazy_blt.jpg'),
+  ('Grilled Cheese, Bacon and Oven-Dried Tomato Sandwich', 'This is grilled cheese and whole nuther level', 'Lunch', 1200, 'images/Grilled-Cheese-Bacon-Oven-Dried-Tomato-Sandwich.jpg'),
+  ('Chicken & Bacon Tart', 'If you’re one of those folks who think bacon is a food group, you’ll love this weeknight-friendly twist with a sweet and spicy kick from jalapeno jelly.', 'Dinner', 1100, 'images/chicken-bacon-tart.jpg'),
+  ('Bacon meets bun', 'You will be truly satified with the work of bacon', 'Lunch' 900, '/images/best-bacon-burger.jpg'),
+  ('Bacon and eggs', 'A classic breakfast of eggs your way, bacon, toast, and home fries.', 'Breakfast', 895, '/images/two-eggs-sunny-side-up.jpg')
