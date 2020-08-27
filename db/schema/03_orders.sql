74921e75bbd1548b5d35a4b1ad125301cb09a83b
Patrick Mitchener <pmitchener@rogers.com>
2020-08-27 14:56:43 +0000

    Forgot to save the 03_orders.sql schema file.

--- db/schema/03_orders.sql
+++ db/schema/03_orders.sql
@@ -16,6 +16,7 @@ CREATE TABLE order_details (
   id SERIAL PRIMARY KEY NOT NULL,
   order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
   item_id INTEGER REFERENCES items(id) ON DELETE CASCADE, /* not sure about this. if you delete an item you cannot delete the line from the order detail. Soooo, do not delete items??*/
+  description VARCHAR(255),
   quantity SMALLINT NOT NULL,
   price INTEGER NOT NULL
 );
